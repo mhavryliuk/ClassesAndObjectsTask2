99fb1e9e8e61dc44d62a204f650bc309876e85ab
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-10-13 18:06:56 +0300

    Начал работу над заданием 2 по теме Классы и объекты

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassesAndObjectsTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Triangle firstTriangle = new Triangle(10,6,15);
+            Console.WriteLine(firstTriangle);
+            Console.WriteLine(firstTriangle.CalcPerimeter());
+            Console.WriteLine(firstTriangle.CalcArea());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ClassesAndObjectsTask2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassesAndObjectsTask2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bda9084f-3771-4e7b-90ff-a0da3acd590a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
